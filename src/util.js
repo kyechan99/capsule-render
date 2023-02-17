a080dd8d18402122d92bc7c91de040c604833ee8
Kauê Reis <kaue0607@gmail.com>
2023-02-17 22:14:53 +1300

    Fix bug related to replaceAll support

--- src/util.js
+++ src/util.js
@@ -88,7 +88,7 @@ function checkText(text, fontColor = '000000', fontAlign = '50', fontAlignY = '5
         return '';
     
     // debate : adjustable text-anchor|pos-y. not only pos-x
-    return `<text text-anchor="middle" alignment-baseline="middle" x="${fontAlign}%" y="${fontAlignY}%" class="text" style="fill:#${fontColor};" stroke="#${stroke}" stroke-width="${strokeWidth}" >${text.replaceAll('%0', '<br>')}</text>`;
+    return `<text text-anchor="middle" alignment-baseline="middle" x="${fontAlign}%" y="${fontAlignY}%" class="text" style="fill:#${fontColor};" stroke="#${stroke}" stroke-width="${strokeWidth}" >${text.replace(/%0/g, '<br>')}</text>`;
 }
 
 function checkDesc(desc, descColor = '000000', descAlign = '50', descAlignY = '60') {
