1aea2ba69f075c60cc5c101bfec64c0a02f8e9d0
Kauê Reis <kaue0607@gmail.com>
2023-02-18 05:32:40 +1300

    line break debug

--- src/util.js
+++ src/util.js
@@ -99,7 +99,6 @@ function checkText(text, fontColor = '000000', fontAlign = '50', fontAlignY = '5
 
     // debate : adjustable text-anchor|pos-y. not only pos-x
     return lines.map((line, i) => {
-        (fontAlignY[0] || fontAlignY || firstAlignY)
         alignY.push(fontAlignY[i] || (Number(alignY[i-1] || firstAlignY) + lineSpace));
         
         return `<text text-anchor="middle" alignment-baseline="middle" x="${fontAlign}%" y="${alignY[i]}%" class="text" style="fill:#${fontColor};" stroke="#${stroke}" stroke-width="${strokeWidth}" >${line}</text>`
