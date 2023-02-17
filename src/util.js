3788f410872a9695dce5bdab7d88e22f3b492dd9
Kauê Reis <kaue0607@gmail.com>
2023-02-18 06:09:25 +1300

    Update util.js

--- src/util.js
+++ src/util.js
@@ -102,7 +102,7 @@ function checkText(text, fontColor = '000000', fontAlign = '50', fontAlignY = '5
         alignY.push(
             typeof fontAlignY !== 'string' && fontAlignY[i] ? fontAlignY[i] 
             : alignY[i-1] ? (Number(alignY[i-1]) + lineSpace) 
-            : fontAlignY;
+            : 50;
         );
         
         return `<text text-anchor="middle" alignment-baseline="middle" x="${fontAlign}%" y="${alignY[i]}%" class="text" style="fill:#${fontColor};" stroke="#${stroke}" stroke-width="${strokeWidth}" >${line}</text>`
